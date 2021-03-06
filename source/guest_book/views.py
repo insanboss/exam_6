c91b3ed41703e97b1bd81c62cd0d7f9278bcc0fa
Insan <insanboss@gmail.com>
2021-03-06 16:40:31 +0600

    Сделан вывод карточек и добавлен вывод сообщений об ошибках.

--- source/guest_book/views.py
+++ source/guest_book/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from guest_book.models import Page
+
 
 # Create your views here.
+
+
+def index_view(request):
+    pages = Page.objects.filter(status="active").order_by('-created_at')
+    context = {
+        'pages': pages
+    }
+    return render(request, 'index.html', context)
